c41e9ec4c2f920e4cf672a097ad22e2cb3e41c95
Home <home@MacBook-Pro-de-Home.local>
2022-08-13 17:19:34 -0300

    calculadora final

--- index.js
+++ index.js
@@ -1,26 +1,16 @@
-// const dias = document.querySelector('#dias')
-// pegar o valor do input de dias 
-
-const salario_hora = document.querySelector('#salario_hora')
-// pegar o valor do input salario hora
-
-
-
-// function multiplicaSalario () {
-   
-//         const dias = document.getElementById('dias').value;
-//         const salario_hora = document.getElementById('salario_hora').value;
-//         const totalSalario = dias*salario_hora
-//         console.log(totalSalario)
-    
-// }
-// document.querySelector('#totalSalario').innerHTML = multiplicaSalario ();
-
-function multiSalario(){
-    const dias = document.getElementById('dias').value
-    const salario = document.getElementById('salario_hora').value
-    const total = dias * salario
-    const salarioFinal = document.querySelector('#total').innerHTML += total
-    return salarioFinal
+const calcularSalario = () => {
+    const diasTrabalhados = document.querySelector('#dias').value
+    const valorHora = documente.querySelector ('#salario_hora').value
+    const valorDia = valorHora * 8 
+    const salarioMes = valorDia * diasTrabalhados
+    const salarioTotal = document.querySelector('#salarioBruto')
+    salarioTotal.innerHTML = 'R$' + salarioMes.toFixed(2)
 }
 
+function calcularValorHora () {
+    const diasTrabalhados = document.querySelector('#dias').value
+    const salarioTotal = document.querySelector('#salario_total').value
+    const valorHora = (salarioTotal / diasTrabalhados) / horasDiarias
+    const salarioHora = document.querySelector('#valor_hora')
+    salarioHora.innerHTML = 'R$ ' + valorHora.toFixed(2)
+}
